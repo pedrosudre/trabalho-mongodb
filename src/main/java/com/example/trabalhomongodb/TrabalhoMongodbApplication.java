8c4e71f9d52ebfb51ee2d183466a60d16c479c3f
Pedro Sudré <pedrosudre04@gmail.com>
2023-06-01 07:18:33 -0300

    first commit

---
+++ src/main/java/com/example/trabalhomongodb/TrabalhoMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.example.trabalhomongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabalhoMongodbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrabalhoMongodbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/trabalhomongodb/TrabalhoMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.trabalhomongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrabalhoMongodbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
